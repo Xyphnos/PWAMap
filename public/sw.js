42cf832d5624284d95b815a5dcd74c448bf43708
Xyphnos <arttuva@gmail.com>
2020-04-19 16:54:18 +0300

    this works apparently?

--- public/sw.js
+++ public/sw.js
@@ -7,6 +7,17 @@ let filesToCache = [
     './img/icon.png',
     './img/logo.svg',
     './img/splash.png',
+    "./modules/esri-leaflet/dist/esri-leaflet.js",
+    "./modules/esri-leaflet-geocoder/dist/esri-leaflet-geocoder.js",
+    "./modules/esri-leaflet-geocoder/dist/esri-leaflet-geocoder.css",
+    "./modules/leaflet/dist/leaflet.js",
+    "./modules/leaflet/dist/leaflet.css",
+    "./modules/leaflet/dist/images/marker-icon.png",
+    "./modules/leaflet/dist/images/marker-shadow.png",
+    "./modules/@fortawesome/fontawesome-free/css/all.min.css",
+    "./modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff2",
+    "./modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.woff",
+    "./modules/@fortawesome/fontawesome-free/webfonts/fa-solid-900.ttf"
 ];
 
 self.addEventListener('install', (e) => {
